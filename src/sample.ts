0cae51af3d99d57bb907bfb7dacc87de1a76e0a2
AntonioJesusRG <antoniogar129@gmail.com>
2023-01-31 20:17:19 +0100

    Add too much things :(

---
+++ src/sample.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-new */
+import { Header } from './components/component/component';
+
+console.log('hola caracola');
+new Header('#root');
---
+++ src/tasks.ts
@@ -0,0 +1,7 @@
+// Aqui se crea la estructura base que se usa en data.ts
+export type Taskstructure = {
+  id: number;
+  title: string;
+  responsible: string;
+  isComplete: boolean;
+};
